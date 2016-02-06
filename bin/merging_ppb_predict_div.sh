ff304b3a318c73c1f35e13df3d9bc058e8d8cad5
Jordan Hayes <jhayes@ucr.edu>
2016-02-05 16:22:43 -0800

    Merging script now iterates over each DMP file within secified path

--- bin/merging_ppb_predict_div.sh
+++ bin/merging_ppb_predict_div.sh
@@ -3,30 +3,41 @@
 EVOLVER="evolver"
 BASEDIR="$(dirname $0)/.."
 
+
 # Set current directory to whatever is passed in
 if [[ ! -z "$1" ]]; then
-    echo "$1" 
     cd "$1"
 fi
 
 # Define important files
-PPBFILE=$(ls *PPB2.fe); 
-DMPLOWEST=$(ls *.dmp | grep -oP '[0-9]+\.[0-9]+\.dmp$' | sed 's/\.dmp//g' | sort -n | head -1);
-DMPFILE=$(ls *${DMPLOWEST}.dmp)
+PPBFILE=$(ls *PPB2.fe 2>/dev/null | head -1)
+CELLNAME=${PWD##*/}
+if [[ -z $PPBFILE ]]; then
+    echo -e "${CELLNAME}\tERROR\tNO PPBFILE"
+    exit
+fi
 
 # Run through each fe file and merge PPB and predictive division
-for fe in $(ls *-30.fe); do
-    # Merge all sources into a single fe file
-    #echo "addload \"${DMPFILE}\";" > ${fe}_merged.fe
-    #cat $fe ${BASEDIR}/etc/maybealittleok.txt ${BASEDIR}/etc/PPBaccuracyGREEN_Realz.txt | sed "s/PPBFILE/${PPBFILE}/g" | sed "s/DMPFILE/${DMPFILE}/g" | sed "s/showq//g" > ${BASEDIR}/results/${fe}_merged.fe;
-    cat ${DMPFILE} ${BASEDIR}/etc/PPBaccuracyGREEN_Realz.txt | sed "s/PPBFILE/${PPBFILE}/g" | sed "s/showq//g" | sed "s/ee\.e_boundary/0/g" > ${BASEDIR}/results/${fe}_merged.fe;
-    #cat $fe ${BASEDIR}/etc/ghost.txtt ${BASEDIR}/etc/PPBaccuracyGREEN_Realz.txt | sed "s/PPBFILE/${PPBFILE}/g" > ${BASEDIR}/results/${fe}_merged.fe;
-    #cat ${BASEDIR}/etc/PPBaccuracyGREEN_Realz.txt | sed "s/PPBFILE/${PPBFILE}/g" > ${BASEDIR}/results/${fe}_merged.fe;
+for DMPFILE in $(ls *.dmp); do
+    # Get uniq name, by energy metric
+    DMP=$(echo ${DMPFILE} | grep -oP '[0-9]+\.[0-9]+\.dmp$' | sed 's/\.dmp//g');
+    
+    # Merge all sources into a single fe file and replace VARs and remove broken code
+    mkdir -p ${BASEDIR}/results/${CELLNAME}
+    cat ${DMPFILE} ${BASEDIR}/etc/PPBaccuracyGREEN_Realz.txt | sed "s/PPBFILE/${PPBFILE}/g" | sed "s/showq//g" | sed "s/ee\.e_boundary/0/g" > ${BASEDIR}/results/${CELLNAME}/${DMP}_merged
 
     # Run evolver with fe and data files
-    echo ${BASEDIR}/results/${fe}_merged.fe | evolver | tail -2 | head -1 | grep -oP "[0-9\.]+"
-    #echo ${EVOLVER} -f${BASEDIR}/results/${fe}_merged.fe ${DMPFILE} 
-    #${EVOLVER} -f${DMPFILE} ${BASEDIR}/results/${fe}_merged.fe
-    #${EVOLVER} -f${BASEDIR}/results/${fe}_merged.fe ${DMPFILE}
+    PRED=$(echo ${BASEDIR}/results/${CELLNAME}/${DMP}_merged | ${EVOLVER} 2>${BASEDIR}/results/${CELLNAME}/${DMP}_error.log | tail -2 | head -1 | grep -oP "[0-9\.]+")
+    #echo "${BASEDIR}/results/${CELLNAME}/${DMP}_merged" | ${EVOLVER}
+    
+    # Print out Name and Prediction
+    ERROR=$(cat ${BASEDIR}/results/${CELLNAME}/${DMP}_error.log | wc -l)
+    if [[ ! "${ERROR}" -gt "0" ]]; then
+        echo -e "${CELLNAME}\t${DMP}\t${PRED}"
+        rm -f ${BASEDIR}/results/${CELLNAME}/${DMP}_error.log
+    else
+        echo -e "${CELLNAME}\t${DMP}\tERROR"
+    fi
+
 done
 
