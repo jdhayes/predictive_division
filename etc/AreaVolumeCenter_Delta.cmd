f04dc49b467a7a16cef9836d8d1480b377df9583
Jordan Hayes <hayesjordan@gmail.com>
2019-10-17 20:44:09 -0700

    Updated abnormality index formulas

--- etc/AreaVolumeCenter_Delta.cmd
+++ etc/AreaVolumeCenter_Delta.cmd
@@ -3,8 +3,8 @@ procedure area_volume_center_delta() {
     area_center_y := sum(facet ff,ff.area*avg(ff.vertex,y))/total_area;
     area_center_z := sum(facet ff,ff.area*avg(ff.vertex,z))/total_area;
     vol_center_x := sum(facet ff, ff.x*avg(ff.vertex,x^2/2))/body[1].volume;
-    vol_center_y := sum(facet ff, ff.x*avg(ff.vertex,y^2/2))/body[1].volume;
-    vol_center_z := sum(facet ff, ff.x*avg(ff.vertex,z^2/2))/body[1].volume;
+    vol_center_y := sum(facet ff, ff.y*avg(ff.vertex,y^2/2))/body[1].volume;
+    vol_center_z := sum(facet ff, ff.z*avg(ff.vertex,z^2/2))/body[1].volume;
     dist := sqrt((area_center_x-vol_center_x)^2
                + (area_center_y-vol_center_y)^2
                + (area_center_z-vol_center_z)^2);
